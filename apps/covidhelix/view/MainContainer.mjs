68c7177dba71d18a9c6d9628530858a914cb3fc9
tobiu <tobiasuhlig78@gmail.com>
2020-03-14 19:00:16 +0100

    MainContainer

--- apps/covidhelix/view/MainContainer.mjs
+++ apps/covidhelix/view/MainContainer.mjs
@@ -1,6 +1,7 @@
-import {default as Component}    from '../../../node_modules/neo.mjs/src/component/Base.mjs';
-import {default as TabContainer} from '../../../node_modules/neo.mjs/src/tab/Container.mjs';
-import Viewport                  from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
+import CountryHelix            from './CountryHelix.mjs';
+import {default as Panel}      from '../../../node_modules/neo.mjs/src/container/Panel.mjs';
+import {default as RangeField} from '../../../node_modules/neo.mjs/src/form/field/Range.mjs';
+import Viewport                from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
 
 /**
  * @class CovidHelix.view.MainContainer
@@ -12,35 +13,368 @@ class MainContainer extends Viewport {
         ntype    : 'main-container',
 
         autoMount: true,
-        layout   : {ntype: 'fit'},
+        /**
+         * @member {String[]} cls=['neo-helix-maincontainer', 'neo-viewport']
+         */
+        cls: ['neo-helix-maincontainer', 'neo-viewport'],
+        /**
+         * @member {Neo.component.Helix|null} helix=null
+         */
+        helix: null,
+        /**
+         * @member {Object|null} helixConfig=null
+         */
+        helixConfig: null,
+        layout: {ntype: 'hbox', align: 'stretch'},
 
         items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
+            ntype : 'container',
+            flex  : 1,
+            layout: 'fit',
+            items : []
+        }, {
+            ntype : 'panel',
+            cls   : ['neo-controls-panel', 'neo-panel', 'neo-container'],
+            layout: {ntype: 'vbox',align: 'stretch'},
+            style : {backgroundColor: '#2b2b2b'},
+            width : 250,
 
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
+            containerConfig: {
+                flex : null,
+                style: {overflowY: 'scroll'}
             },
 
+            itemDefaults: {
+                ntype        : 'rangefield',
+                flex         : '0 1 auto',
+                labelWidth   : '100px',
+                style        : {padding: '10px'},
+                useInputEvent: true,
+
+                listeners: {
+                    change: function(data) {
+                        if (['deltaY', 'maxOpacity', 'minOpacity'].includes(this.name)) {
+                            data.value /= 100;
+                        }
+                        Neo.get('neo-helix-1')[this.name] = data.value;
+                    }
+                }
+            },
+
+            headers: [{
+                dock: 'top',
+                items: [{
+                    ntype: 'button',
+                    text : 'X',
+                    handler: function() {
+                        const panel  = this.up('panel'),
+                              expand = panel.width === 40;
+
+                        panel.width = expand ? 250 : 40;
+                        this.text   = expand ? 'X' : '+';
+                    }
+                }, {
+                    ntype: 'label',
+                    text : 'Helix Controls'
+                }]
+            }],
+
             items: [{
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
+                labelText: 'Translate X',
+                maxValue : 2000,
+                minValue : -2000,
+                name     : 'translateX',
+                value    : 400
             }, {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
+                labelText: 'Translate Y',
+                maxValue : 2500,
+                minValue : -2500,
+                name     : 'translateY',
+                value    : -350
+            }, {
+                labelText: 'Translate Z',
+                maxValue : 4500,
+                minValue : -4500,
+                name     : 'translateZ',
+                value    : -1500,
+                listeners: {
+                    change: function(data) {
+                        Neo.get('neo-helix-1')[this.name] = data.value;
+                    },
+                    mounted: function(fieldId) {
+                        let field = Neo.get(fieldId);
+
+                        Neo.get('neo-helix-1').on('changeTranslateZ', function(value) {
+                            value = Math.min(Math.max(value, this.minValue), this.maxValue);
+                            this.value = value;
+                        }, field);
+                    }
+                }
+            }, {
+                labelText : 'Delta Y',
+                labelAlign: 'top',
+                maxValue  : 600,
+                minValue  : -600,
+                name      : 'deltaY',
+                value     : 70
+            }, {
+                labelText: 'Rotate',
+                minValue : -720,
+                maxValue : 720,
+                name     : 'rotationAngle',
+                value    : 0,
+                listeners: {
+                    change: function(data) {
+                        Neo.get('neo-helix-1')[this.name] = data.value;
+                    },
+                    mounted: function(fieldId) {
+                        let field = Neo.get(fieldId);
+
+                        Neo.get('neo-helix-1').on('changeRotation', function(value) {
+                            value = Math.min(Math.max(value, this.minValue), this.maxValue);
+                            this.value = value;
+                        }, field);
+                    }
+                }
+            }, {
+                labelText: 'Radius',
+                maxValue : 3500,
+                minValue : 900,
+                name     : 'radius',
+                value    : 1500
+            }, {
+                labelText: 'Perspective',
+                minValue : 50,
+                maxValue : 3000,
+                name     : 'perspective',
+                value    : 800
+            }, {
+                labelText: 'Item Angle',
+                minValue : 1,
+                maxValue : 36,
+                name     : 'itemAngle',
+                value    : 8
+            }, {
+                labelText: 'Max Opacity',
+                name     : 'maxOpacity',
+                minValue : 0,
+                maxValue : 100,
+                value    : 80 // todo [30, 80]
+            }, {
+                labelText: 'Min Opacity',
+                name     : 'minOpacity',
+                minValue : 0,
+                maxValue : 100,
+                value    : 30
+            }, {
+                ntype     : 'button',
+                text      : 'Flip Items',
+                listeners: {},
+                style    : {margin: '20px'},
+                domListeners: {
+                    click: data => {
+                        const helix = Neo.get('neo-helix-1');
+                        helix.flipped = !helix.flipped;
+                    }
+                }
+            }, {
+                ntype: 'label',
+                text : 'Sort By:'
+            }, {
+                ntype : 'container',
+                layout: {ntype: 'hbox', align: 'stretch'},
+                style : {padding: '0'},
+                items : [{
+                    ntype : 'container',
+                    layout: {ntype: 'vbox', align: 'stretch'},
+                    items : [{
+                        ntype    : 'button',
+                        text     : 'Cases',
+                        listeners: {},
+                        style    : {margin: '10px', marginTop: '0'},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'cases',
+                                    direction: 'DESC'
+                                }];
+                            }
+                        }
+                    }, {
+                        ntype    : 'button',
+                        text     : 'Deaths',
+                        listeners: {},
+                        style    : {margin: '10px', marginBottom: '10px', marginTop: 0},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'deaths',
+                                    direction: 'DESC'
+                                }];
+                            }
+                        }
+                    }, {
+                        ntype    : 'button',
+                        text     : 'Country',
+                        listeners: {},
+                        style    : {margin: '10px', marginTop: 0},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'country',
+                                    direction: 'ASC'
+                                }];
+                            }
+                        }
+                    }]
+                }, {
+                    ntype : 'container',
+                    layout: {ntype: 'vbox', align: 'stretch'},
+                    items : [{
+                        ntype    : 'button',
+                        text     : 'Cases today',
+                        listeners: {},
+                        style    : {margin: '10px', marginTop: '0'},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'todayCases',
+                                    direction: 'DESC'
+                                }];
+                            }
+                        }
+                    }, {
+                        ntype    : 'button',
+                        text     : 'Deaths today',
+                        listeners: {},
+                        style    : {margin: '10px', marginBottom: '10px', marginTop: 0},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'todayDeaths',
+                                    direction: 'DESC'
+                                }];
+                            }
+                        }
+                    }, {
+                        ntype    : 'button',
+                        text     : 'Critical',
+                        listeners: {},
+                        style    : {margin: '10px', marginTop: 0},
+
+                        domListeners: {
+                            click: function() {
+                                Neo.get('neo-helix-1').store.sorters = [{
+                                    property : 'critical',
+                                    direction: 'DESC'
+                                }];
+                            }
+                        }
+                    }]
+                }]
+            }, {
+                ntype    : 'button',
+                iconCls  : 'fa fa-square',
+                text     : 'Follow Selection',
+                listeners: {},
+                domListeners: {
+                    click: function(data) {
+                        let me   = this,
+                            helix = Neo.get('neo-helix-1');
+
+                        if (me.iconCls === 'fa fa-square') {
+                            helix.followSelection = true;
+                            me.iconCls = 'fa fa-check-square';
+                        } else {
+                            helix.followSelection = false;
+                            me.iconCls = 'fa fa-square';
+                        }
+                    }
                 },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
+                style: {
+                    margin      : '20px',
+                    marginBottom: '10px'
+                }
+            }, {
+                ntype: 'label',
+                text : [
+                    '<b>Navigation Concept</b>',
+                    '<p>Click on an item to select it. Afterwards you can use the Arrow Keys to walk through the items.</p>',
+                    '<p>Hit the Space Key to rotate the currently selected item to the front.</p>',
+                    '<p>Hit the Enter Key to expand the currently selected item.</p>'
+                ].join(''),
+
+                style: {
+                    backgroundColor: '#323232',
+                    color          : '#ddd',
+                    fontSize       : '13px',
+                    margin         : '10px',
+                    padding        : '10px',
+                    whiteSpace     : 'normal'
+                }
+            }, {
+                ntype: 'label',
+                cls  : ['neo-link-color'],
+                text : [
+                    '<b>Attribution</b>',
+                    '<p>App created with <a href="https://github.com/neomjs/neo">neo.mjs</a>.</p>',
+                    '<p>Data provided by <a href="https://github.com/NovelCOVID/API">NovelCOVID/API</a>.</p>',
+                    '<p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>.</p>'
+                ].join(''),
+
+                style: {
+                    backgroundColor: '#323232',
+                    color          : '#ddd',
+                    fontSize       : '13px',
+                    margin         : '10px',
+                    padding        : '10px',
+                    whiteSpace     : 'normal'
+                }
             }]
         }]
     }}
+
+    /**
+     *
+     * @param {Object} config
+     */
+    constructor(config) {
+        super(config);
+
+        const me       = this,
+              proxyUrl = "https://cors-anywhere.herokuapp.com/",
+              url      = 'https://corona.lmao.ninja/countries';
+
+        me.helix = Neo.create({
+            module: CountryHelix,
+            id    : 'neo-helix-1',
+            ...me.helixConfig || {}
+        });
+
+        me.items[0].items.push(me.helix);
+
+        fetch(proxyUrl + url)
+            .then(response => response.json())
+            .then(data => me.addStoreItems(data))
+            .catch(err => console.log('Can’t access ' + url, err));
+    }
+
+    addStoreItems(data) {
+        this.getStore().data = data;
+    }
+
+    /**
+     *
+     * @returns {Neo.data.Store}
+     */
+    getStore() {
+        return this.items[0].items[0].store;
+    }
 }
 
 Neo.applyClassConfig(MainContainer);
